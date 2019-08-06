5d02cc91ba001dfd655290bb832f395e41393db9
Nate Cope <nathanielrcope@gmail.com>
2019-08-05 21:19:11 -0400

    bare skeleton

---
+++ Exercise.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+"""
+TODO
+"""
+
+# imports go here
+
+class Wordification_Effort:
+    """" TODO """
+    def __init__(self, phone_number):
+        """ TODO """
+        self.phone_number = phone_number
+
+    def number_to_words(self, provided_phone_number_string):
+        """
+            takes as an argument a string representing a US phone
+            number and which outputs a string which has transformed part or all of the phone
+            number into a single "wordified" phone number that can be typed on a US telephone
+        """
+        print("")
+        return
+
+    def words_to_number(self, wordification):
+        """
+            Outputs full-number version of a given wordification
+        """
+        print("")
+        return
+
+    def all_wordifications(self):
+        """
+            Outputs all possible wordifications of a given number
+        """
+        print("")
+        return
+
+
+def main():
+    """ nice UI and some tests probably """
+
+
+if __name__ == "__main__":
+    main()
